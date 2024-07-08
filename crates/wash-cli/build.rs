d9682549b0eda677504d24bc26679b0e0e732c1e
Victor Adossi <vadossi@cosmonic.com>
2024-07-09 01:59:52 +0900

    fix: check for windows
    Signed-off-by: Victor Adossi <vadossi@cosmonic.com>

--- crates/wash-cli/build.rs
+++ crates/wash-cli/build.rs
@@ -1,5 +1,6 @@
 use std::time::Duration;
 
+use anyhow::{Context as _, Result};
 use tokio::join;
 use tokio::net::{lookup_host, TcpStream};
 use tokio::time::timeout;
@@ -21,10 +22,25 @@ async fn record_reachability(host: &str) {
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<()> {
+    // On Windows, `clap` can overflow the stack in debug mode due to small default stack size
+    //
+    // This generally triggers during the `integration_help_subcommand_check` which checks the help
+    // command (i.e. walking the entire arg tree)
+    //
+    // see:
+    // - https://github.com/clap-rs/clap/issues/5134
+    // - https://github.com/clap-rs/clap/issues/4516
+    let target_env = std::env::var("CARGO_CFG_TARGET_ENV").context("missing cargo cfg target")?;
+    if target_env == "msvc" {
+        println!("cargo:rustc-link-arg=/stack:{}", 8 * 1024 * 1024);
+    }
+
     join!(
         record_reachability("github.com"),
         record_reachability("ghcr.io"),
         record_reachability("wasmcloud.azurecr.io"),
     );
+
+    Ok(())
 }
